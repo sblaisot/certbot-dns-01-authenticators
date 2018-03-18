a5d215fbf205cc0bf5ceaa2365a71961a7e7a5f1
Sebastien BLAISOT <sebastien.blaisot@cozycloud.cc>
2018-03-18 13:03:12 +0100

    Add LICENSE

--- local_bind/auth.sh
+++ local_bind/auth.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 ZONE_FILE=/etc/bind/zones/db.${CERTBOT_DOMAIN}
 
 if [ ! -f ${ZONE_FILE} ]; then
@@ -32,7 +45,6 @@ echo "Update serial"
 sed -i -e 's/^.*\; serial number of this zone file$/                        '${NEW_SERIAL}'      ; serial number of this zone file/' ${ZONE_FILE}
 
 echo "Add challenge to zone file"
-
 echo "_acme-challenge		IN	TXT	${CERTBOT_VALIDATION}" >> ${ZONE_FILE}
 
 echo "Release zone"
